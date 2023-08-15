eb0524654439b5f0f39de475827fea80e1a1d10f
Pama Lee <pama@pamalee.cn>
2023-08-15 22:26:30 +0800

    feat: 人员更新

--- src/views/MembersView.vue
+++ src/views/MembersView.vue
@@ -50,14 +50,6 @@ const memberDatas = [
         department: 'DEVELOPMENT',
         avatar: 'https://s1.ax1x.com/2023/05/10/p9rnkO1.jpg',
     },{
-        name: 'GU MENGHAN',
-        department: 'DEVELOPMENT',
-        avatar: 'https://s1.ax1x.com/2023/05/11/p9ru334.jpg'
-    },{
-        name: 'PENG XINYUN',
-        department: 'DEVELOPMENT',
-        avatar: 'https://i.niupic.com/images/2023/05/11/b6Ch.jpg'
-    },{
         name: 'QI YUJUN',
         department: 'DEVELOPMENT',
         avatar: 'https://s1.ax1x.com/2023/05/14/p9cr1b9.jpg'
